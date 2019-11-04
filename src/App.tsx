464cdcad77550558d3188f102be2af4bed270ec2
Alex Trzeciak <trebek08@gmail.com>
2019-11-04 00:00:31 -0800

    change landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 const App: React.FC = () => {
   return (
@@ -8,19 +8,12 @@ const App: React.FC = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Living in SF and Looking for Vegan options downtown has always been
+          difficult. Thus, this site was born.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
-}
+};
 
 export default App;
