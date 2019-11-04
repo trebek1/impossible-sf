0103fbf6c6d72dc61e8efaeccfc672ba0411bbe3
Alex Trzeciak <trebek08@gmail.com>
2019-11-04 00:01:11 -0800

    make implicit return

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,16 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Living in SF and Looking for Vegan options downtown has always been
-          difficult. Thus, this site was born.
-        </p>
-      </header>
-    </div>
-  );
-};
+const App: React.FC = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Living in SF and Looking for Vegan options downtown has always been
+        difficult. Thus, this site was born.
+      </p>
+    </header>
+  </div>
+);
 
 export default App;
