7868f9b02b5d3cb38914fea7acc978f654cdc80a
Alex Trzeciak <trebek08@gmail.com>
2019-11-09 21:09:19 -0800

    unpack create react app and add image

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
+import vegan from "./vegan.jpg";
 import "./App.css";
 
 const App: React.FC = () => (
   <div className="App">
     <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
+      <img src={vegan} className="App-logo" alt="logo" />
       <p>
         Living in SF and Looking for Vegan options downtown has always been
         difficult. Thus, this site was born.
